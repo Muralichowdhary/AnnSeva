71aa6c1aef6ef35b972dc81d182cde93d3a557b7
Muralichowdhary <134221183+Muralichowdhary@users.noreply.github.com>
2024-08-04 09:04:25 +0530

    Update otpRoutes.js

--- Backend/Routes/otpRoutes.js
+++ Backend/Routes/otpRoutes.js
@@ -7,9 +7,13 @@ router.post('/', async (req, res) => {
   try {
     console.log("hello");
     const { email } = req.body;
+    console.log("hello1")
     const result = await sendOtp({ email });
+    console.log("hello2")
     res.status(200).json(result);
+    console.log("hello3")
   } catch (error) {
+    console.log("hello4")
     res.status(500).json({ message: error.message });
   }
 });

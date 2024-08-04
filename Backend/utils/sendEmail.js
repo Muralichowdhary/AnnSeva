4ae367d1362a15e89998475d99faa81fc09e0c8b
Muralichowdhary <134221183+Muralichowdhary@users.noreply.github.com>
2024-08-04 09:27:57 +0530

    Update sendEmail.js

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,25 +1,35 @@
 const nodemailer = require("nodemailer");
+require('dotenv').config()
 const { AUTH_EMAIL, AUTH_PASS } = process.env;
 let transporter = nodemailer.createTransport({
   host: "smtp-mail.outlook.com",
-  // port:587,
-  // secure:false,
+  port:587,
+  secure:false,
   auth: {
     user: AUTH_EMAIL,
     pass: AUTH_PASS,
   },
+  tls: {
+    ciphers: 'SSLv3'
+  }
+  
 });
 
 transporter.verify((error, success) => {
   if (error) {
     console.log(error);
   }
+  else {
+    console.log('connection established successfully')
+  }
+  
 });
 
 const sendEmail = async (mailOptions) => {
   try {
     await transporter.sendMail(mailOptions);
   } catch (error) {
+    console.log(error)
     throw error;
   }
 };

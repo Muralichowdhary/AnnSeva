44ccdb94af56fa29b79a8bbe1761a7d1df673444
Muralichowdhary <134221183+Muralichowdhary@users.noreply.github.com>
2024-08-04 09:56:51 +0530

    Update sendEmail.js

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -1,37 +1,35 @@
 const nodemailer = require("nodemailer");
-require('dotenv').config()
+require('dotenv').config();
+
 const { AUTH_EMAIL, AUTH_PASS } = process.env;
+
 let transporter = nodemailer.createTransport({
-  host: "gmail",
-  port:587,
-  secure:false,
+  service: 'gmail',
   auth: {
-    user: "k663385@gmail.com"
+    user: "k663385@gmail.com",
     pass: "kagra@143209",
   },
-  tls: {
-    ciphers: 'SSLv3'
-  }
- 
 });
- console.log("a")
+
+console.log("a");
+
 transporter.verify((error, success) => {
   if (error) {
-     console.log("b")
+    console.log("b");
     console.log(error);
+  } else {
+    console.log('Connection established successfully');
   }
-  else {
-    console.log('connection established successfully')
-  }
-  
 });
- console.log("c")
+
+console.log("c");
+
 const sendEmail = async (mailOptions) => {
   try {
     await transporter.sendMail(mailOptions);
-     console.log("d")
+    console.log("d");
   } catch (error) {
-    console.log(error)
+    console.log(error);
     throw error;
   }
 };
